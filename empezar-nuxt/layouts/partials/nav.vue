06ce709a149db802275bf6177666b83e63598ce0
Antonio G <antonioe@niurons.com>
2019-04-23 14:32:07 -0500

    inicio

---
+++ empezar-nuxt/layouts/partials/nav.vue
@@ -0,0 +1,48 @@
+<template>
+    <v-toolbar
+    fixed
+    app
+    >
+    <v-toolbar-items>
+        <v-btn flat>Vision Tec</v-btn>
+    </v-toolbar-items>
+      <!-- <v-toolbar-side-icon @click="drawer = !drawer" />
+      <v-btn
+        icon
+        @click.stop="miniVariant = !miniVariant"
+      >
+        <v-icon>{{ `chevron_${miniVariant ? 'right' : 'left'}` }}</v-icon>
+      </v-btn>
+      <v-btn
+        @click.stop="clipped = !clipped"
+      >
+        Vision Tec
+      </v-btn>
+      <nuxt-link to="/">Vision Tec</nuxt-link>
+      <v-btn
+        icon
+        @click.stop="fixed = !fixed"
+      >
+        <v-icon>remove</v-icon>
+      </v-btn>
+      <v-toolbar-title v-text="title" />
+      <v-spacer />
+      <v-btn
+        icon
+        @click.stop="rightDrawer = !rightDrawer"
+      >
+        <v-icon>menu</v-icon>
+      </v-btn> -->
+      <!-- <header>
+        <nuxt-link to="/" class="logo">Nuxty</nuxt-link>
+        <nav>
+            <ul>
+                <li><nuxt-link to="/">Home</nuxt-link></li>
+                <li><nuxt-link to="about">About</nuxt-link></li>
+                <li><nuxt-link to="services">Services</nuxt-link></li>
+                <li><nuxt-link to="contact">Contact</nuxt-link></li>
+            </ul>
+        </nav>
+    </header> -->
+    </v-toolbar>
+</template>
